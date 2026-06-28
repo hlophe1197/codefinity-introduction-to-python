556a13c47435b95c1b7500901ae0f99d5efc41aa
hlophe1197 <>
2026-06-28 19:16:39 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,13 @@
+
+
+
+# Testing
+item_name = "mango"
+item_price = 1.50
+item_stock_quantity = 50
+in_stock = True
+
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
