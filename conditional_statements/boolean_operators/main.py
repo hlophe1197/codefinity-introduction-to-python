8c6e25b9a7cfc528770b1a4f8d533f76a1044a1f
hlophe1197 <>
2026-07-15 10:35:07 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,9 @@
+#Define the total cost and the discount eligibility
+total_cost = 25.00
+discount_threshold = 20.00
+
+#Check if the total cost is above the discount_Eligibilty
+discountEligible = total_cost >= discount_threshold
+
+#Print the result
+print("Is the purchase eligible for a discount?", discountEligible)
